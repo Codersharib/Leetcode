61e093c888e7f976c2419da6e3b5b9a4982368b1
Codersharib <codersharib@gmail.com>
2026-07-05 01:15:35 +0530

    Time: 3 ms (33.13%), Space: 49.4 MB (5.5%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,19 +2,18 @@ class Solution {
     static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
     public List<String> letterCombinations(String digits) {
         List<String> ll=new ArrayList<>();
-        letter_comb_ph_no(digits,"",ll);
+        print(digits,"",ll);
         return ll;
     }
-    public static void letter_comb_ph_no(String ques,String ans,List<String> ll){
-        if(ques.length()==0){
-            // System.out.println(ans);
+    public static void print(String que,String ans,List<String> ll){
+        if(que.length()==0){
             ll.add(ans);
             return;
         }
-        char ch=ques.charAt(0);
+        char ch=que.charAt(0);
         String press=key[ch-'0'];
         for(int i=0;i<press.length();i++) {
-            letter_comb_ph_no(ques.substring(1), ans+press.charAt(i),ll);
-        }        
+            print(que.substring(1), ans+press.charAt(i), ll);
+        }
     }
 }
