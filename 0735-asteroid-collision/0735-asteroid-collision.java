5500365591724c516cf41fbc9357ffa8ffa76e1d
Codersharib <codersharib@gmail.com>
2026-08-17 01:33:36 +0530

    Time: 4 ms (82.85%), Space: 47.2 MB (30.58%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -6,27 +6,28 @@ class Solution {
     public static int[] collision(int[] arr) {
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
-            boolean self_killed = false;
-            while (!st.isEmpty() && arr[i] < 0 && arr[st.peek()] > 0) {
+            boolean self_kill=false;
+            while (!st.isEmpty() && arr[i] < 0 && arr[st.peek()]>0) {
                 if (arr[st.peek()] < -arr[i]) {
                     st.pop();
                 } else if (arr[st.peek()] == -arr[i]) {
                     st.pop();
-                    self_killed = true;
+                    self_kill=true;
                     break;
-                } else {
-                    self_killed = true;
+                }
+                else{
+                    self_kill=true;
                     break;
                 }
             }
-            if (!self_killed) {
+            if(!self_kill){
                 st.push(i);
             }
         }
-        int[] ans = new int[st.size()];
-        int i = ans.length - 1;
-        while (!st.isEmpty()) {
-            ans[i--] = arr[st.pop()];
+        int[] ans=new int[st.size()];
+        int i=st.size()-1;
+        while(!st.isEmpty()){
+            ans[i--]=arr[st.pop()];
         }
         return ans;
     }
