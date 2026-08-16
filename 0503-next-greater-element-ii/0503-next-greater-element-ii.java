9e77f634a663a2f3196c23dc7c7c45addaf5ea8b
Codersharib <codersharib@gmail.com>
2026-08-17 02:17:32 +0530

    Time: 9 ms (88.99%), Space: 48.6 MB (48.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -5,7 +5,6 @@ class Solution {
     public static int[] NGE2(int[] arr){
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[arr.length];
-        int turn=0;
         for (int i = 0; i< 2*arr.length; i++) {
             int idx=i%arr.length;
             while (!st.isEmpty() && arr[idx] > arr[st.peek()]) {
