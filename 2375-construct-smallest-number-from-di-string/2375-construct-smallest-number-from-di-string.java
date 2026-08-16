ed653622605e1305ac98235aa557a2f08cfdcaca
Codersharib <codersharib@gmail.com>
2026-08-16 22:18:04 +0530

    Time: 5 ms (5.7%), Space: 43.1 MB (28.19%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
@@ -1,26 +1,23 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        return construct_smallest_no_DI(pattern);
+        return DI(pattern);
     }
-    public static String construct_smallest_no_DI(String pattern){
-        int[] arr=new int[pattern.length()+1];
+    public static String DI(String s){
         Stack<Integer> st=new Stack<>();
+        int[] ans=new int[s.length()+1];
         int c=1;
-        for(int i=0;i<=pattern.length();i++) {
-            if(i==pattern.length() || pattern.charAt(i)=='I'){
-                arr[i]=c++;
+        for (int i = 0; i < ans.length; i++) {
+            if(i==s.length() || s.charAt(i)=='I'){
+                ans[i]=c++;
                 while(!st.isEmpty()){
-                    arr[st.pop()]=c++;
+                    ans[st.pop()]=c++;
                 }
             }
             else{
                 st.push(i);
             }
         }
-        StringBuilder sb=new StringBuilder();
-        for(int x:arr){
-            sb.append(x);
-        }
-        return sb.toString();
+        String res=Arrays.stream(ans).mapToObj(String::valueOf).collect(Collectors.joining());
+        return res;
     }
 }
