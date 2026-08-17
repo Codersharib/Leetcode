7c60167dcabb1aae8b5277b940a3459d2c860cd9
Codersharib <codersharib@gmail.com>
2026-08-18 00:13:41 +0530

    Time: 37 ms (77.77%), Space: 97.4 MB (89.83%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -5,16 +5,14 @@ class Solution {
     public static boolean pattern132(int[] arr){
         Stack<Integer> st=new Stack<>();
         int third=Integer.MIN_VALUE;
-        for(int i=arr.length-1;i>=0;i--) {
-            if(arr[i]<third){
-                return true;
-            } 
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]<third) return true;
             while(!st.isEmpty() && arr[i]>arr[st.peek()]){
                 third=arr[st.pop()];
             }
             st.push(i);
         }
         return false;
-        
     }
+        
 }
