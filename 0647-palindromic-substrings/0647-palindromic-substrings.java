a518401f7b4361e2b99cc464c1ef12903ecbf808
Codersharib <codersharib@gmail.com>
2026-08-25 23:56:39 +0530

    Time: 8 ms (42.48%), Space: 42.8 MB (68.69%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,19 +4,18 @@ class Solution {
     }
     public static int axis_orbit(String s){
         int odd=0;
-        for(int axis=0;axis<s.length();axis++){
-            for(int orbit=0;axis-orbit>=0 && axis+orbit<s.length();orbit++){
-                if(s.charAt(axis-orbit)!=s.charAt(axis+orbit)){
+        for (int axis = 0; axis < s.length(); axis++) {
+            for (int orbit = 0; axis+orbit < s.length() && axis-orbit>=0; orbit++) {
+                if(s.charAt(axis-orbit) != s.charAt(axis+orbit)){
                     break;
                 }
                 odd++;
             }
         }
-        
         int even=0;
-        for(double axis=0.5;axis<s.length();axis++){
-            for(double orbit=0.5;axis-orbit>=0 && axis+orbit<s.length();orbit++){
-                if(s.charAt((int)(axis-orbit))!=s.charAt((int)(axis+orbit))){
+        for (double axis = 0; axis < s.length(); axis++) {
+            for (double orbit = 0.5; axis+orbit < s.length() && axis-orbit>=0; orbit++) {
+                if(s.charAt((int)(axis-orbit)) != s.charAt((int)(axis+orbit))){
                     break;
                 }
                 even++;
