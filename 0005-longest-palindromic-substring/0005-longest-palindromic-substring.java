0f25bed232f0c4015aa981db69f60c4849ae729d
Codersharib <codersharib@gmail.com>
2026-08-27 02:19:29 +0530

    Time: 104 ms (30.26%), Space: 47.4 MB (10.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,20 +2,19 @@ class Solution {
     public String longestPalindrome(String s) {
         return LPS(s);
     }
-    public static String LPS(String ques){
+    public static String LPS(String s){
         // int odd=0;
         StringBuilder sb_o=new StringBuilder();
         StringBuilder sb=new StringBuilder();
-        for(int axis=0;axis<ques.length();axis++) {
-            for(int orbit=0;axis-orbit>=0 && axis+orbit<ques.length();orbit++) {
-                if(ques.charAt(axis-orbit) != ques.charAt(axis+orbit)){
+        for (int axis = 0; axis < s.length(); axis++) {
+            for (int orbit = 0; axis+orbit < s.length() && axis-orbit>=0; orbit++) {
+                if(s.charAt(axis-orbit) != s.charAt(axis+orbit)){
                     break;
                 }
-                if(axis-orbit == axis+orbit) sb.append(ques.charAt(axis+orbit));
+                if(axis-orbit==axis+orbit) sb.append(s.charAt(axis+orbit));
                 else{
-                    sb.insert(0,ques.charAt(axis-orbit));
-                    sb.append(ques.charAt(axis+orbit));
-
+                    sb.insert(0,s.charAt(axis-orbit));
+                    sb.append(s.charAt(axis+orbit));
                 }
                 // odd++;
             }
@@ -27,15 +26,15 @@ class Solution {
         // int even=0;
         StringBuilder sb_e=new StringBuilder();
         StringBuilder sb1=new StringBuilder();
-        for(double axis=0.5;axis<ques.length();axis++) {
-            for(double orbit=0.5;axis-orbit>=0 && axis+orbit<ques.length();orbit++) {
-                if(ques.charAt((int)(axis-orbit)) != ques.charAt((int)(axis+orbit))){
+        for (double axis = 0.5; axis < s.length(); axis++) {
+            for (double orbit = 0.5; axis+orbit < s.length() && axis-orbit>=0; orbit++) {
+                if(s.charAt((int)(axis-orbit)) != s.charAt((int)(axis+orbit))){
                     break;
                 }
-                if((int)(axis-orbit) == (int)(axis+orbit)) sb1.append(ques.charAt((int)(axis+orbit)));
+                if((int)(axis-orbit)== (int)(axis+orbit)) sb1.append(s.charAt((int)(axis+orbit)));
                 else{
-                    sb1.insert(0,ques.charAt((int)(axis-orbit)));
-                    sb1.append(ques.charAt((int)(axis+orbit)));
+                    sb1.insert(0,s.charAt((int)(axis-orbit)));
+                    sb1.append(s.charAt((int)(axis+orbit)));
                 }
                 // even++;
             }
@@ -43,8 +42,7 @@ class Solution {
                 sb_e=new StringBuilder(sb1.toString());
             }
             sb1.setLength(0);
-
         }
-        return sb_o.length()>sb_e.length()?sb_o.toString():sb_e.toString();
+        return sb_e.length()>sb_o.length()? sb_e.toString():sb_o.toString();
     }
 }
