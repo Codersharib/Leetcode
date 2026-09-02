e676e8d670372256fbc9377d5360663d72a82086
Codersharib <codersharib@gmail.com>
2026-09-03 01:28:48 +0530

    Time: 0 ms (100%), Space: 44.9 MB (79.9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,27 +14,14 @@
  * }
  */
 class Solution {
+    long prev=Long.MIN_VALUE;
     public boolean isValidBST(TreeNode root) {
-        return validBST(root).isbst;
-    }
-    public bstpair validBST(TreeNode root){
-        if(root==null) return new bstpair();
-        bstpair lbp=validBST(root.left);
-        bstpair rbp=validBST(root.right);
-        bstpair sbp=new bstpair();
-        sbp.min=Math.min(root.val,Math.min(lbp.min,rbp.min));
-        sbp.max=Math.max(root.val,Math.max(lbp.max,rbp.max));
-        if(lbp.isbst && rbp.isbst && lbp.max<root.val && rbp.min>root.val){
-            sbp.isbst=true;
-        }
-        else {
-            sbp.isbst=false;
-        }
-        return sbp;
-    }
-    class bstpair{
-        boolean isbst=true;
-        long max=Long.MIN_VALUE;
-        long min=Long.MAX_VALUE;
+        if(root==null) return true;
+        // left
+        if(!isValidBST(root.left)) return false;
+        // current
+        if(root.val<=prev) return false;
+        prev=root.val;
+        return isValidBST(root.right);
     }
 }
