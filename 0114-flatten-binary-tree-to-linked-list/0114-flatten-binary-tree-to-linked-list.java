6b1335d51b52c541995cbbade5d9022b487cd976
Codersharib <codersharib@gmail.com>
2026-09-02 17:25:26 +0530

    Time: 0 ms (100%), Space: 44.1 MB (34.12%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,18 +15,18 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        MakeLL(root);
+        flatten_(root);
     }
-    public TreeNode MakeLL(TreeNode root){
-        if(root == null) return null;
+    public TreeNode flatten_(TreeNode root) {
+        if(root==null) return null;
         if(root.left==null && root.right==null) return root;
-        TreeNode left_tail=MakeLL(root.left);
-        TreeNode right_tail=MakeLL(root.right);
+        TreeNode left_tail=flatten_(root.left);
+        TreeNode right_tail=flatten_(root.right);
         if(left_tail!=null){
             left_tail.right=root.right;
             root.right=root.left;
             root.left=null;
         }
-        return right_tail!=null ? right_tail:left_tail;
+        return right_tail!=null? right_tail:left_tail;
     }
 }
